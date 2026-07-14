5665ef78c4cfeaecf83188694bef88302d338e15
Thiagomiranda91 <36204706+Thiagomiranda91@users.noreply.github.com>
2026-07-14 09:43:55 +0100

    Add files via upload

---
+++ components/sampleData.js
@@ -0,0 +1,24 @@
+export const SAMPLE_VISITORS = [
+  { key: "2026-02", month: "Feb", visitors: 0 },
+  { key: "2026-03", month: "Mar", visitors: 0 },
+  { key: "2026-04", month: "Apr", visitors: 0 },
+  { key: "2026-05", month: "May", visitors: 0 },
+  { key: "2026-06", month: "Jun", visitors: 0 },
+  { key: "2026-07", month: "Jul", visitors: 0 },
+];
+
+export const SAMPLE_NEW_RETURNING = [
+  { segment: "new", users: 0 },
+  { segment: "returning", users: 0 },
+];
+
+export const SAMPLE_TOP_PAGES = [];
+
+export const SAMPLE_SEARCH_QUERIES = [];
+
+export const SAMPLE_KEY_EVENTS = {
+  total: 0,
+  byEvent: [],
+};
+
+export const SAMPLE_CHANNELS = [];
